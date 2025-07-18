ef0281ed14ed700e74bd12f1ef63650eb171bf87
Paulo Cidro <paulocidro@gmail.com>
2025-07-18 11:11:36 -0300

    animar scroll

--- script.js
+++ script.js
@@ -154,3 +154,20 @@ formulario.addEventListener("submit", function (e) {
 });
 
 // Animação ao Scroll
+
+const sections = document.querySelectorAll(".js-scroll");
+if (sections.length) {
+  const windowMetade = window.innerHeight * 0.7;
+
+  function animaScroll() {
+    sections.forEach((section) => {
+      const sectionTop = section.getBoundingClientRect().top - windowMetade;
+      if (sectionTop < 0) {
+        section.classList.add("scroll-ativo");
+      }
+    });
+  }
+  animaScroll();
+
+  window.addEventListener("scroll", animaScroll);
+}
